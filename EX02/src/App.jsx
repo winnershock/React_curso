75811394e6e9d78c80c3dec8271aaa7db8ea4d03
winnershock <winnersolarte749@gmail.com>
2025-10-31 09:57:06 -0500

    cambios realizados

---
+++ EX02/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <h1> Hola mundo lleno de personas</h1>
+      <p>welcome to my react app aplication</p>
+    </>
+  )
+}
